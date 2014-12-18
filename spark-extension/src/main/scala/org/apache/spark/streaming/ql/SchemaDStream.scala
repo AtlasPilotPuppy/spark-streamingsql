63d9e16a299244d529514f342251ccebddcf00e9
jerryshao <saisai.shao@intel.com>
2014-12-18 11:05:09 +0800

    Add SchemaDStream support

---
+++ spark-extension/src/main/scala/org/apache/spark/streaming/ql/SchemaDStream.scala
@@ -0,0 +1,156 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.streaming.ql
+
+import scala.collection.mutable.ArrayBuffer
+
+import org.apache.spark.annotation.Experimental
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SchemaRDD
+import org.apache.spark.sql.catalyst.expressions._
+import org.apache.spark.sql.catalyst.plans.{Inner, JoinType}
+import org.apache.spark.sql.catalyst.plans.logical._
+import org.apache.spark.sql.catalyst.types.StructType
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.{Duration, Time}
+
+/**
+ * :: Experimental ::
+ * A row based DStream with schema involved, offer user the ability to manipulate SQL or
+ * LINQ-like query on DStream, it is similar to SchemaRDD, which offers the similar function to
+ * users. Internally, SchemaDStream treat rdd of each batch duration as a small table, and force
+ * query on this small table.
+ *
+ * The SQL function offered by SchemaDStream is a subset of standard SQL or HiveQL, currently it
+ * doesn't support INSERT, CTAS like query in which queried out data need to be written into another
+ * destination.
+ */
+@Experimental
+class SchemaDStream(
+    val qlConnector: StreamQLConnector,
+    val baseLogicalPlan: LogicalPlan)
+  extends DStream[Row](qlConnector.streamContext) {
+
+  override def dependencies = physicalStream.toList
+
+  override def slideDuration: Duration = physicalStream.head.slideDuration
+
+  override def compute(validTime: Time): Option[RDD[Row]] = {
+    // Set the valid batch duration for this rule to get correct RDD in DStream of this batch
+    // duration
+    PhysicalDStream.setValidTime(validTime)
+    // Scan the streaming logic plan to convert streaming plan to specific RDD logic plan.
+    Some(new SchemaRDD(qlConnector.qlContext, preAnalyzedPlan))
+  }
+
+  private lazy val preAnalyzedPlan = qlConnector.analyzePlan(baseLogicalPlan)
+
+  private lazy val physicalStream = {
+    val tmp = ArrayBuffer[DStream[Row]]()
+    preAnalyzedPlan.foreach(_ match {
+        case LogicalDStream(_, stream) => tmp += stream
+        case _ => Unit
+      }
+    )
+    tmp
+  }
+
+  /**
+   * Returns the schema of this SchemaDStream (represented by a [[StructType]]]).
+   */
+  lazy val schema: StructType = preAnalyzedPlan.schema
+
+  // Streaming DSL query
+
+  /**
+   * Changes the output of this relation to the given expressions, similar to the `SELECT` clause
+   * in SQL.
+   */
+  def select(exprs: Expression*): SchemaDStream = {
+    val aliases = exprs.zipWithIndex.map {
+      case (ne: NamedExpression, _) => ne
+      case (e, i) => Alias(e, s"c$i")()
+    }
+    new SchemaDStream(qlConnector, Project(aliases, baseLogicalPlan))
+  }
+
+  /**
+   * Filters output, only returning those rows where `condition` evaluates to true.
+   */
+  def where(condition: Expression): SchemaDStream = {
+    new SchemaDStream(qlConnector, Filter(condition, baseLogicalPlan))
+  }
+
+  /**
+   * Performs a relational join on two SchemaDStreams
+   */
+  def join(otherPlan: SchemaDStream,
+           joinType: JoinType = Inner,
+           on: Option[Expression] = None): SchemaDStream = {
+    new SchemaDStream(qlConnector,
+      Join(baseLogicalPlan, otherPlan.baseLogicalPlan, joinType, on))
+  }
+
+  /**
+   * Performs a relational join between SchemaDStream and SchemaRDD
+   */
+  def tableJoin(otherPlan: SchemaRDD,
+                joinType: JoinType = Inner,
+                on: Option[Expression] = None): SchemaDStream = {
+    new SchemaDStream(qlConnector,
+      Join(baseLogicalPlan, otherPlan.logicalPlan, joinType, on))
+  }
+
+  /**
+   * Sorts the results by the given expressions.
+   */
+  def orderBy(sortExprs: SortOrder*): SchemaDStream =
+    new SchemaDStream(qlConnector, Sort(sortExprs, baseLogicalPlan))
+
+  /**
+   * Limits the results by the given integers.
+   */
+  def limit(limitNum: Int): SchemaDStream =
+    new SchemaDStream(qlConnector, Limit(Literal(limitNum), baseLogicalPlan))
+
+  /**
+   * Performs a grouping followed by an aggregation.
+   */
+  def groupBy(groupingExprs: Expression*)(aggregateExprs: Expression*): SchemaDStream = {
+    val aliasedExprs = aggregateExprs.map {
+      case ne: NamedExpression => ne
+      case e => Alias(e, e.toString)()
+    }
+    new SchemaDStream(qlConnector, Aggregate(groupingExprs, aliasedExprs, baseLogicalPlan))
+  }
+
+  /**
+   * Performs an aggregation over all Rows in this DStream. This is equivalent to groupBy with
+   * no grouping expressions.
+   */
+  def aggregate(aggregateExprs: Expression*): SchemaDStream = {
+    groupBy()(aggregateExprs: _*)
+  }
+
+  /**
+   * Applies a qualifier to the attributes of this relation.
+   */
+  def as(alias: Symbol) =
+    new SchemaDStream(qlConnector, Subquery(alias.name, baseLogicalPlan))
+
+}
